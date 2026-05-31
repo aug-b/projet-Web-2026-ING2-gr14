a6b84dd2b9d32cb521b33b5858e9df6b87e03b5c
MaelR-prog <mael.ronzier@edu.ece.fr>
2026-05-31 19:30:41 +0200

    Delete caca directory

--- caca/script-connexion.js
+++
@@ -1,9 +0,0 @@
-console.log("SmartCampus chargé");
-const params = new URLSearchParams(window.location.search);
-
-if(params.get("error") === "1"){
-
-    document.getElementById("message-erreur").innerText =
-    "Email ou mot de passe incorrect";
-
-}
